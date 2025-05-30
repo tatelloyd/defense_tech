2fe9a12ccd4b8e9dc3c465af82aff899c470454e
Tate Lloyd <tatewlloyd@gmail.com>
2025-05-30 15:35:19 -0600

    Added unsaved changes

--- spacecraft-rtos-sim/target/debug/deps/spacecraft_rtos_sim-fe8104596b23971a.d
+++ spacecraft-rtos-sim/target/debug/deps/spacecraft_rtos_sim-fe8104596b23971a.d
@@ -1,6 +1,6 @@
-/Users/tatelloyd/Desktop/Software Projects/Defense_Tech_Projects/spacecraft-rtos-sim/target/debug/deps/spacecraft_rtos_sim-fe8104596b23971a: src/main.rs src/rtos/mod.rs src/rtos/scheduler.rs src/rtos/task.rs src/spacecraft/mod.rs src/spacecraft/attitude_control.rs src/spacecraft/navigation.rs src/spacecraft/telemetry.rs src/drivers/mod.rs src/drivers/sensor_driver.rs src/utils/mod.rs src/utils/metrics.rs src/utils/math.rs
+/Users/tatelloyd/Desktop/Software Projects/Defense_Tech_Projects/spacecraft-rtos-sim/target/debug/deps/spacecraft_rtos_sim-fe8104596b23971a: src/main.rs src/rtos/mod.rs src/rtos/scheduler.rs src/rtos/task.rs src/spacecraft/mod.rs src/spacecraft/attitude_control.rs src/spacecraft/navigation.rs src/spacecraft/telemetry.rs src/spacecraft/housekeeping.rs src/drivers/mod.rs src/drivers/sensor_driver.rs src/utils/mod.rs src/utils/metrics.rs src/utils/math.rs
 
-/Users/tatelloyd/Desktop/Software Projects/Defense_Tech_Projects/spacecraft-rtos-sim/target/debug/deps/spacecraft_rtos_sim-fe8104596b23971a.d: src/main.rs src/rtos/mod.rs src/rtos/scheduler.rs src/rtos/task.rs src/spacecraft/mod.rs src/spacecraft/attitude_control.rs src/spacecraft/navigation.rs src/spacecraft/telemetry.rs src/drivers/mod.rs src/drivers/sensor_driver.rs src/utils/mod.rs src/utils/metrics.rs src/utils/math.rs
+/Users/tatelloyd/Desktop/Software Projects/Defense_Tech_Projects/spacecraft-rtos-sim/target/debug/deps/spacecraft_rtos_sim-fe8104596b23971a.d: src/main.rs src/rtos/mod.rs src/rtos/scheduler.rs src/rtos/task.rs src/spacecraft/mod.rs src/spacecraft/attitude_control.rs src/spacecraft/navigation.rs src/spacecraft/telemetry.rs src/spacecraft/housekeeping.rs src/drivers/mod.rs src/drivers/sensor_driver.rs src/utils/mod.rs src/utils/metrics.rs src/utils/math.rs
 
 src/main.rs:
 src/rtos/mod.rs:
@@ -10,6 +10,7 @@ src/spacecraft/mod.rs:
 src/spacecraft/attitude_control.rs:
 src/spacecraft/navigation.rs:
 src/spacecraft/telemetry.rs:
+src/spacecraft/housekeeping.rs:
 src/drivers/mod.rs:
 src/drivers/sensor_driver.rs:
 src/utils/mod.rs:
